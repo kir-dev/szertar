0ad21ce7f2fd8b0b334a04a160f7482c1150eaf2
Zsolt Fazekas <fazekas.zsolt95@gmail.com>
2018-03-25 18:53:21 +0200

    edit item count

--- middlewares/item/getItem.js
+++ middlewares/item/getItem.js
@@ -2,8 +2,6 @@ var objectRepository = require('../../models/objectRepository');
 
 module.exports = function () {
     var items = objectRepository.itemModel;
-
-
     return function (req, res, next) {
         items.findOne({
             _id: req.body.id
--- middlewares/item/updateItem.js
+++ middlewares/item/updateItem.js
@@ -4,24 +4,15 @@ var itemModel = objectRepository.itemModel;
 module.exports = function () {
 
     return function (req, res, next) {
+              
+        var count = req.body.count; 
         
-        var id=req.body.id;
-        var count = req.body.count;
-        console.log(count);
-        console.log(item);
-        var item = req.item;
-        var newItem;
-        if (!item) {
-            return next();
-          //  itemModel.create({name: req.body.newItem, count: count}).then(() => {return next();});
-        } else {
-            item.count = parseInt(count, 10);
-            newItem = item;
-        }
-        req.item = newItem;
-        item =newItem;
-        item.save(function(err) {
-            if (err) return console.log(err);
+        if (!req.item) {
+            return next();         
+        }          
+        req.item.count = parseInt(count, 10);                            
+        req.item.save(function(err) {
+            if (err) return next(err);
             return next();
         });
         return next();
