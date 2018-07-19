1c79a94a02f75d5518cfe3a00704d9f2dc965da2
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2018-07-19 18:21:17 +0200

    Add stock edit option
    -Added stock edit
    -Fixed bug in editItem.js

--- middlewares/item/editItem.js
+++ middlewares/item/editItem.js
@@ -6,16 +6,17 @@ module.exports = function () {
     return function (req, res, next) {
 
         var count = req.body.count;
+        var stock = req.body.stock;
         var name = req.body.name;
         if (!req.item) {
             return next();
         }
         req.item.count = parseInt(count, 10);
         req.item.name = name;
+        req.item.stock = parseInt(stock, 10);
         req.item.save(function (err) {
             if (err) return next(err);
             return next();
         });
-        return next();
     }
 };
