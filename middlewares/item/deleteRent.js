1a52b5ceb042509a3e66c7a58a3f32bf0cfb9c2f
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2018-08-02 22:53:48 +0200

    Update icons, fix bug in deleteRent

--- middlewares/item/deleteRent.js
+++ middlewares/item/deleteRent.js
@@ -1,15 +1,16 @@
 var objectRepository = require('../../models/objectRepository');
 var rentModel = objectRepository.rentModel;
-var userModel = objectRepository.userModel
 
 module.exports = function () {
     return function (req, res, next) {
         rentModel.findById(req.params.id).populate({path: 'items._id', model: 'Item'}).populate({path: 'user', model: 'User'}).exec((err, res) => {
-            res.items.forEach(item => {
-                item._id.stock += item.amount
-                item._id.save()
-            })
-            res.save()
+            if(res.state >= 3){
+                res.items.forEach(item => {
+                    item._id.stock += item.amount
+                    item._id.save()
+                })
+                res.save()
+            }
             rentModel.findByIdAndRemove(
                 req.params.id
              , function (err) {
